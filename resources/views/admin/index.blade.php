92d11501078021e933f65a00a4121fc42682be0a
Balintataw J <balintataw2@gmail.com>
2019-01-30 10:40:00 -0800

    Up-versioned to Laravel 5.7

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,14 +3,22 @@
 @section('content')
 
     <h3>Admin Dashboard</h3>
-    <canvas id="myChart" height="100"></canvas>
+    <div class="row">
+        <div class="col-sm-6">
+            <canvas id="bar-graph"></canvas>
+        </div>
+        <div class="col-sm-6">
+            <canvas id="pie-chart"></canvas>
+        </div>
+    </div>
 
 @stop
 
 @section('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 <script>
-    var ctx = document.getElementById("myChart").getContext('2d');
+    // bar graph
+    var ctx = document.getElementById("bar-graph").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -42,6 +50,30 @@
                 }]
             }
         }
+    });
+    // pie chart
+    var ctxpie = document.getElementById('pie-chart').getContext('2d');
+    var myPieChart = new Chart(ctxpie,{
+        type: 'pie',
+        data: {
+            labels: ['Users', 'Posts', 'Comments', 'Categories'],
+            datasets: [{
+                data: [{{ $userCount }}, {{ $postCount }}, {{ $commentCount }}, {{ $categoryCount }}],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+            }],
+        },
+        options: {}
     });
 </script>
 @stop
